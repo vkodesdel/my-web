a07ed8cd5eb76d8b12a4c1a41895b316cf9fae11
vkodesdel <victorwandiga@gmail.com>
2026-05-08 16:44:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function sayHello() {
+  alert("Yo! JavaScript is alive! 🔥");
+}
+
+function changeName() {
+  document.querySelector("h1").textContent = "I am a Developer! 🚀";
+}
+
+function changeColor() {
+  document.body.style.backgroundColor = "purple";
+}
